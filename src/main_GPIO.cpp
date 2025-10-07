47308bfeeeba975aefdf702c19dde9a7dcd6046b
CATITA <catita.enzo.pro@gmail.com>
2025-10-07 10:41:16 +0200

    Code Faux

--- src/main_GPIO.cpp
+++ src/main_GPIO.cpp
@@ -11,3 +11,4 @@ digitalWrite(led, HIGH);// Ecrit la valeur HIGH (=1) sur la broche GPIO16
 delay(500); // Attendre 500ms
 digitalWrite(led, LOW); // Ecrit la valeur LOW (=0) sur la broche GPIO16
 delay(500);
+}
