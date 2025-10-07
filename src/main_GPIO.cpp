9f2cd761c03254829856af7286fc580b1686e842
CATITA <catita.enzo.pro@gmail.com>
2025-10-07 11:22:31 +0200

    Identation du code

--- src/main_GPIO.cpp
+++ src/main_GPIO.cpp
@@ -1,14 +1,14 @@
 /////////// LED Clignotante //////////////
 #include <Arduino.h>
-int led = 16;//Déclaration de la variable led, comme un entier (integer)
+int led = 16; // Déclaration de la variable led, comme un entier (integer)
 void setup()
 {
-pinMode(led, OUTPUT); // Configure la broche GPIO16 en sortie
+    pinMode(led, OUTPUT); // Configure la broche GPIO16 en sortie
 }
 void loop()
 {
-digitalWrite(led, HIGH);// Ecrit la valeur HIGH (=1) sur la broche GPIO16
-delay(500); // Attendre 500ms
-digitalWrite(led, LOW); // Ecrit la valeur LOW (=0) sur la broche GPIO16
-delay(500);
+    digitalWrite(led, HIGH); // Ecrit la valeur HIGH (=1) sur la broche GPIO16
+    delay(500);              // Attendre 500ms
+    digitalWrite(led, LOW);  // Ecrit la valeur LOW (=0) sur la broche GPIO16
+    delay(500);
 }
